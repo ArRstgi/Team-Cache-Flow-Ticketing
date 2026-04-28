be02260c55930a0a4e4f9ed4e09e4f1782d5a265
me <93354442+dlbrown1223@users.noreply.github.com>
2026-04-27 20:40:56 -0400

    better frontend functionality

--- frontend/app.js
+++ frontend/app.js
@@ -4,44 +4,33 @@
  */
 
 import express from "express";
-import { createClient } from 'redis'
+// import { createClient } from 'redis'
+import service_data_object from './service_data.json' with { type: 'json' };
 
 // get env variables
 const SERVICE_INNER_PORT = Number(process.env.INNER_PORT || "3000");
 const SERVICE_OUTER_PORT = Number(process.env.OUTER_PORT || "80");
-const redisUrl = process.env.REDIS_URL || "redis://redis:6379";
+// const redisUrl = process.env.REDIS_URL || "redis://redis:6379";
 
 
-// get array of service urls
-const serviceMap = {};
-const serviceData = (process.env.SERVICE_URLS || "")
-    .split(" ")
-    .filter(str=>str) //nonempty
-    .map((str,index)=>{
-        const serv_str = str.split(","); //[ name, url, outer_port, inner_port ]
-        const name = serv_str[0];
-        const url = serv_str[1];
-        const port_outer = serv_str[2];
-        const port_inner = serv_str[3];
+// parse service data
+const serviceData = service_data_object//JSON.parse((process.env.SERVICE_DATA || "[]"))
+    .filter(elem=>elem) //nonempty
+    .map((dat,index)=>{
         return {
-            name,
-            url_inner: `${url}:${port_inner}`,
-            port_inner,
-            port_outer,
-            health_url: `http://localhost:${port_outer}/health`,
+            ...dat,
+            url_inner: `${dat.url}:${dat.port_inner}`,
+            url_outer: `http://localhost:${dat.port_outer}`,
             index,
         };
-    });
-serviceData.forEach(serv=>{
-    serviceMap[serv.name] = serv;
-});
-// console.log(serviceMap);
-
+    })
 
 const app = express();
-const redisClient = createClient({ url: redisUrl });
+// const redisClient = createClient({ url: redisUrl });
 
-
+// frontend page served from /public/index.html
+app.use(express.static("public"));
+app.use(express.json());
 
 /// endpoints
 
@@ -52,129 +41,27 @@ app.get("/health",(req,res)=>{
     });
 });
 
+// get the data describing the services
+app.get("/servicedata",(req,res)=>{
+    res.json(serviceData);
+});
 
-// dashboard for monitoring what's going on
-app.get("/",async (req,res)=>{
+//pipe request to backend, so it works on chrome
+app.post("/fetch_backend",async (req,res)=>{
+    const data = req.body;
+    let response = await fetch(data.url,data.options);
+    
+    try {
+        response = await response.json();
+    }
+    catch(_err){}
 
-    let serv_string = "";
-    const add_report = (success,label,order=0)=>{
-        serv_string += `<div class="status_listing" style="order:${order};">
-            <div>${success ? "✅" : "❌"}</div>
-            <div class="label">${label}</div>
-        </div>`;
-    };
-
-    let events_string = "";
-
-    //get /health from all services
-    const all_requests = serviceData.map(async (serv)=>{
-        const servTitle = `${serv.name} <a href=${serv.health_url} target="_blank">/health</a>`;
-        const healthURL = serv.url_inner + '/health';
-        return new Promise(async (resolve)=>{
-            await fetch(healthURL)
-                .then(async (response)=>{
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data=>{
-                    let status = data.status || "healthy"
-                    let checks = data.checks;
-                    if ((typeof checks) === "object") {
-                        for(const [key,val] of Object.entries(checks)) {
-                            let checkstatus = "???";
-                            if ((typeof val) === "object") {
-                                checkstatus = val.status || checkstatus;
-                            }
-                            status += `\n${key} status: ${checkstatus}`;
-                        }
-                    }
-                    add_report(true,`${servTitle} ${status}`,serv.index);
-                })
-                .catch(err=>{
-                    console.log(`service ${serv.name} fetch error:`,err);
-                    add_report(false,`${servTitle} ${err}`,serv.index);
-                })
-
-            resolve();
-        });
-    });
-    //get redis health
-    all_requests.push(new Promise(async (resolve,reject)=>{
-        try {
-            const pong = await redisClient.ping();
-            if (pong !== 'PONG') throw new Error(`Unexpected response: ${pong}`);
-            //healthy
-            add_report(true,"Redis healthy",-1);
-        }
-        catch(err) {
-            //unhealthy
-            add_report(false,`Redis ${err}`,-1);
-        }
-        resolve();
-    }));
-
-    //get events
-    all_requests.push(new Promise(async (resolve,reject)=>{
-        try {
-            await fetch(serviceMap["Catalog"].url_inner+"/events")
-                .then(response=>{
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data=>{
-                    events_string += JSON.stringify(data,undefined,2);
-                })
-                // .catch(err=>{
-                //     reject(err);
-                // })
-            resolve();
-        }
-        catch(err) {
-            console.log("get /events error",err);
-            events_string += "Unable to get /events";
-            resolve();
-        }
-    }));
-
-    //wait for all responses
-    await Promise.all(all_requests);
-
-    res.send(`
-        <style>
-            .status_list {
-                white-space: break-spaces;
-                display: flex;
-                flex-direction: column;
-                gap: 4px;
-            }
-            .status_listing {
-                background: #0000001f;
-                padding: 4px 6px;
-                display: flex;
-                align-items: center;
-                gap: 6px;
-            }
-        </style>
-        <div>
-            <h2>Events Dashoard</h2>
-
-            <h3>Service Health:</h3>
-            <div class="status_list">${serv_string}</div>
-
-            <h3>Events:</h3>
-            <div class="status_list">${events_string}</div>
-        </div>
-    `);
+    res.json(response);
 });
 
 
 /// start up
-
-await redisClient.connect();
+// await redisClient.connect();
 
 app.listen(SERVICE_INNER_PORT,()=>{
     let portstr = "";
